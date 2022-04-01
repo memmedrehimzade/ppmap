bd0168faca91e1e55cfff13af935230b1bc54a47
memmedrehimzade <36675105+memmedrehimzade@users.noreply.github.com>
2022-04-01 16:37:55 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo [+] Installing the latest version of ppmap
-go install -v github.com/kleiton0x00/ppmap@latest
+go install -v github.com/memmedrehimzade/ppmap@latest
 
 echo [+] Moving the compiled binary to /usr/bin
 cd /home/$USER/go/bin
